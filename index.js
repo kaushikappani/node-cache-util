f9b20bd21519c39bcdb341b616b4235123d9ac01
appanikaushik <kaushikappani@gmail.com>
2025-02-18 14:25:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const redis = require("redis");
+const util = require('util');
+
+class RedisCacheUtil {
+    constructor(redisUrl, ttl, keyFunction) {
+        this.client = redis.createClient({ url: redisUrl, legacyMode: true });
+        this.ttl = ttl;
+        this.keyFunction = keyFunction;
+
+        this.client.on('error', (err) => {
+            console.error('Redis client error:', err);
+        });
+        this.client.on("ready", () => console.log("redis connected from module"));
+        this.client.connect();
+    }
+
+    cache() {
+        return async (req, res, next) => {
+            const key = this.keyFunction(req);
+            try {
+                const getAsync = util.promisify(this.client.get).bind(this.client);
+
+                const cachedData = await getAsync(key);
+                if (cachedData) {
+                    res.json(JSON.parse(cachedData));
+                } else {
+                    const originalSend = res.send.bind(res);
+                    res.send = async (body) => {
+                        await this.client.setEx(key, this.ttl, body);
+                        originalSend(body);
+                    };
+                    next();
+                }
+            } catch (err) {
+                console.error('Redis middleware error:', err);
+                next();
+            }
+        };
+    }
+}
+
+module.exports = RedisCacheUtil;
